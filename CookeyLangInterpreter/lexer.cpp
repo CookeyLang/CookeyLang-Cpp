e9cef5efd9099cdb50685145d13ecb2d40b79665
Junhao Zhang <junhaodev@gmail.com>
2020-11-24 09:11:28 -0600

    Finally ready to lex!

--- CookeyLangInterpreter/lexer.cpp
+++ CookeyLangInterpreter/lexer.cpp
@@ -5,16 +5,27 @@
 
 // functions
 static void bappend(std::vector<Token>& output, int line, int col, TType type, std::string val = "");
-static void bnewline(int& line, int& col);
+
+static void bnewline(int& line, int& col, int& i);
+static void bnext(int& col, int& i);
+
+static bool bmatch(std::string code, int& i, char c);
+
+static bool isNum(char c);
+static bool isAlpha(char c);
+static bool isAlphaNum(char c);
 
 
 std::vector<Token> lexer(std::string code)
 {
-	static int line = 1, col = 1, i = 0;
-	static std::vector<Token> output;
+	int line = 1, col = 1, i = 0;
+	std::vector<Token> output;
 
-	using namespace std::placeholders;
+	using namespace std::placeholders; // _1, _2, etc
 	auto append = std::bind(bappend, output, line, col, _1, _2);
+	auto newline = std::bind(bnewline, line, col, i);
+	auto next = std::bind(bnext, col, i);
+	auto match = std::bind(bmatch, code, i, _1);
 
 	while (VALID) {
 		char curr = code[i];
@@ -24,14 +35,46 @@ std::vector<Token> lexer(std::string code)
 };
 
 
-static void bappend(std::vector<Token>& output, int line, int col, TType type, std::string val = "")
+static void bappend(std::vector<Token>& output, int line, int col, TType type, std::string val)
 {
 	output.push_back(Token(line, col, type, val));
 }
 
-static void bnewline(int& line, int& col)
+static void bnewline(int& line, int& col, int& i)
 {
+	line++;
+	col = 1;
+	i++;
+}
 
+static void bnext(int& col, int& i)
+{
+	col++;
+	i++;
+}
+
+static bool isNum(char c)
+{
+	return c >= '0' && c <= '9';
+}
+
+static bool isAlpha(char c)
+{
+	return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '_';
+}
+
+static bool isAlphaNum(char c)
+{
+	return isAlpha(c) || isNum(c);
+}
+
+static bool bmatch(std::string code, int& i, char c)
+{
+	if (code[(int64_t)i + 1] == c) {
+		i++;
+		return true;
+	}
+	return false;
 }
 
 #undef VALID
