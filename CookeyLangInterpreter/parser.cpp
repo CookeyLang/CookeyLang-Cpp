bbb2b969d3a1d43bfcc5ea2db71062fdbf7146bc
Junhao Zhang <junhaodev@gmail.com>
2020-11-26 14:25:46 -0600

    ready for parsing

--- CookeyLangInterpreter/parser.cpp
+++ CookeyLangInterpreter/parser.cpp
@@ -9,6 +9,9 @@ static bool bmatch(std::vector<Token> tokens, int i, TType first);
 template <typename... T>
 static bool bmatch(std::vector<Token> tokens, int i, TType first, T... other);
 
+static Token badvance(std::vector<Token> tokens, int* i);
+static Token bprevious(std::vector<Token> tokens, int i);
+
 
 void parser(std::vector<Token> tokens, std::string file)
 {
@@ -16,6 +19,8 @@ void parser(std::vector<Token> tokens, std::string file)
 
 	int i = 0;
 	auto match = std::bind_front(bmatch, i, tokens);
+	auto advance = std::bind(badvance, tokens, &i);
+	auto previous = std::bind(bprevious, tokens, i);
 }
 
 
@@ -32,6 +37,17 @@ static bool bmatch(std::vector<Token> tokens, int i, TType first, T... other)
 	if (bmatch(tokens, i, first))
 		return true;
 	return bmatch(other...);
+}
+
+static Token badvance(std::vector<Token> tokens, int* i)
+{
+	if (*i < tokens.size()) *(i)++;
+	return bprevious(tokens, *i);
+}
+
+static Token bprevious(std::vector<Token> tokens, int i)
+{
+	return tokens[i - 1];
 }
 
 #undef VALID
