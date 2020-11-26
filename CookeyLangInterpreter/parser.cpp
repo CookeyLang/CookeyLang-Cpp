6f845b8c81a54d6fb971c080c656d031e7f6ee94
Junhao Zhang <junhaodev@gmail.com>
2020-11-26 16:24:26 -0600

    yet more fixes

--- CookeyLangInterpreter/parser.cpp
+++ CookeyLangInterpreter/parser.cpp
@@ -4,10 +4,10 @@
 #define VALID (i < tokens.size())
 
 // functions
-static bool bmatch(std::vector<Token> tokens, int i, TType first);
+static bool bmatch(std::vector<Token> tokens, int* i, TType first);
 
 template <typename... T>
-static bool bmatch(std::vector<Token> tokens, int i, TType first, T... other);
+static bool bmatch(std::vector<Token> tokens, int* i, TType first, T... other);
 
 static Token badvance(std::vector<Token> tokens, int* i);
 static Token bprevious(std::vector<Token> tokens, int i);
@@ -18,21 +18,25 @@ void parser(std::vector<Token> tokens, std::string file)
 	using namespace std::placeholders;
 
 	int i = 0;
-	auto match = std::bind_front(bmatch, i, tokens);
+	auto match = [=](auto... T) { bmatch(tokens, &i, T...); };
 	auto advance = std::bind(badvance, tokens, &i);
 	auto previous = std::bind(bprevious, tokens, i);
 }
 
 
-static bool bmatch(std::vector<Token> tokens, int i, TType first)
+static bool bmatch(std::vector<Token> tokens, int* i, TType first)
 {
-	if (!VALID)
-		return false;
-	return tokens[i].type == first;
+	if (*i >= tokens.size() && tokens[*i].type == first)
+	{
+		badvance(tokens, i);
+		return true;
+	}
+
+	return false;
 }
 
 template <typename... T>
-static bool bmatch(std::vector<Token> tokens, int i, TType first, T... other)
+static bool bmatch(std::vector<Token> tokens, int* i, TType first, T... other)
 {
 	if (bmatch(tokens, i, first))
 		return true;
@@ -41,13 +45,13 @@ static bool bmatch(std::vector<Token> tokens, int i, TType first, T... other)
 
 static Token badvance(std::vector<Token> tokens, int* i)
 {
-	if (*i < tokens.size()) *(i)++;
+	if (*i < tokens.size()) (*i)++;
 	return bprevious(tokens, *i);
 }
 
 static Token bprevious(std::vector<Token> tokens, int i)
 {
-	return tokens[i - 1];
+	return tokens[(int64_t)i - 1];
 }
 
 #undef VALID
--- CookeyLangInterpreter/token.cpp
+++ CookeyLangInterpreter/token.cpp
@@ -1,9 +1,10 @@
 #include "token.h"
 
-Token::Token(int line, int col, TType type, std::string value)
+Token::Token(int line, int col, std::string file, TType type, std::string value)
 {
 	this->line = line;
 	this->col = col;
+	this->file = file;
 	this->type = type;
 	this->value = value;
 }
